0c3fbc9e056fb766c823d10805cfbecfb3262162
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-28 15:19:57 +0530

    n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule }from '@angular/common/http';
 import { AdminComponent } from './admin/admin.component';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminnavbarComponent } from './adminnavbar/adminnavbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const link:Routes=[
   {
     path:"",component:LoginComponent
   },
   {
-    path:"admindashboard",component:AdminComponent
+    path:"addemployee",component:AdminComponent
   }
 ]
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminComponent
+    AdminComponent,
+    AdminnavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -37,7 +41,10 @@ const link:Routes=[
     MatTabsModule,
     MatInputModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(link),
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
