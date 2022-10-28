84bebae9dca535622fbacee27678cb8236dd4b5f
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-28 13:50:17 +0530

    c

---
+++ src/app/api.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  signinEmp=(data:any)=>{
+    return this.http.post("http://localhost:8080/signinemployee",data)
+  }
+  signinSec=(data:any)=>{
+    return this.http.post("http://localhost:8080/signinsecurity",data)
+  }
+}
